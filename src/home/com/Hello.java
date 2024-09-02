f162e9e2315b7cdebe45cd2b48b00135b81a3483
Htin Aung Linn Latt <htinaunglinlat1303@gmail.com>
2024-09-02 11:17:32 +0630

    version 2

---
+++ src/home/com/Hello.java
@@ -0,0 +1,12 @@
+package home.com;
+
+public class Hello {
+	String stname;
+	Hello(String stu_name) {
+		stname = stu_name;
+	}
+	public void getName() {
+		System.out.println("Hello, this is " + stname);
+	}
+
+}
